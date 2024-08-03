efcfbf9949627f505b27a03a7bb406c82f90aed7
Walter Villalobos <51486395+waltervillalobos@users.noreply.github.com>
2024-08-03 21:04:56 +0000

    Defining project structure

---
+++ src/main/java/com/dbekoder/algorithm/array/dimension/one/InsertionSort.java
@@ -0,0 +1,12 @@
+package com.dbekoder.algorithm.array.dimension.one;
+
+public class InsertionSort {
+
+    public static void main(String[] args){
+
+
+        System.out.println("Hello World Java");
+    }
+    
+
+}
