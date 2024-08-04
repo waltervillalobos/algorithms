29893336a6abc66570c1f7b173e6b28ecd3d5f34
Walter Villalobos <51486395+waltervillalobos@users.noreply.github.com>
2024-08-04 22:23:23 +0000

    Implementing insertion sort

--- src/main/java/com/dbekoder/algorithm/array/dimension/one/InsertionSort.java
+++ src/main/java/com/dbekoder/algorithm/array/dimension/one/InsertionSort.java
@@ -1,11 +1,49 @@
 package com.dbekoder.algorithm.array.dimension.one;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class InsertionSort {
 
-    public static void main(String[] args){
+    private static int[] execute(int[] array){
+
+        for (int i = 1; i < array.length; i++) {
+
+            int value = array[i];
+
+            int j = i - 1;
+
+            // iterates sub array[0,i] 
+            while((j >= 0) && (value < array[j])) {
+
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = value;
+        }
+
+        return array;
+    }
 
 
-        System.out.println("Hello World Java");
+    public static void main(String[] args) {
+
+        int[] numbers = {32, 42, 24, 12, 53, 28, 30, 5, 29};
+        printArray("Numbers array", numbers);
+        
+        int[] sortedNumbers = execute(numbers);
+        printArray("Sorted array", sortedNumbers);
+    }
+
+    private static void printArray(String title, int[] array) {
+
+        String result = Arrays.stream(array)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+
+        System.out.println(title);
+        System.out.println(result);
     }
     
 
